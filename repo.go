66fdc1956ad2c4e264edab17bd794b6aed2cac09
Edgar Sipki <36490979+ZergsLaw@users.noreply.github.com>
2020-04-26 21:53:02 +0300

    Feature/add collect metric (#1)
    * chore: check
    
    * chore: add collect metric

--- repo.go
+++ repo.go
@@ -11,12 +11,13 @@ import (
 // Provides a number of convenient methods for starting a transaction
 // and starting functions and wrapping returnable errors.
 type Repo struct {
-	db  *sql.DB
-	log *zap.Logger
+	db              *sql.DB
+	log             *zap.Logger
+	metricCollector *Metric
 }
 
 // New return new instance Repo.
-func New(db *sql.DB, log *zap.Logger) *Repo {
+func New(db *sql.DB, log *zap.Logger, m *Metric) *Repo {
 	return &Repo{
 		db:  db,
 		log: log,
@@ -27,48 +28,51 @@ func New(db *sql.DB, log *zap.Logger) *Repo {
 // If the options are not sent, the transaction will start with default parameters.
 // If the callback returns the error, it will be wrapped and enriched with
 // information about where the transaction was called from.
-// TODO: Add collect metric.
+// Automatically collects metrics for function calls.
 func (r *Repo) Tx(ctx context.Context, fn func(*sql.Tx) error, opts ...TxOption) error {
 	methodName := callerMethodName()
 
-	txOption := &sql.TxOptions{}
-	for i := range opts {
-		opts[i](txOption)
-	}
-
-	tx, err := r.db.BeginTx(ctx, txOption)
-	if err != nil {
-		return fmt.Errorf("%s: %w", methodName, err)
-	}
-
-	err = fn(tx)
-	if err != nil {
-		if errRollback := tx.Rollback(); errRollback != nil {
-			err = fmt.Errorf("roolback err: %w", errRollback)
+	return r.metricCollector.collect(methodName, func() error {
+		txOption := &sql.TxOptions{}
+		for i := range opts {
+			opts[i](txOption)
 		}
 
-		return fmt.Errorf("%s: %w", methodName, err)
-	}
+		tx, err := r.db.BeginTx(ctx, txOption)
+		if err != nil {
+			return fmt.Errorf("%s: %w", methodName, err)
+		}
 
-	err = tx.Commit()
-	if err != nil {
-		return fmt.Errorf("%s: %w", methodName, err)
-	}
+		err = fn(tx)
+		if err != nil {
+			if errRollback := tx.Rollback(); errRollback != nil {
+				err = fmt.Errorf("roolback err: %w", errRollback)
+			}
 
-	return nil
+			return fmt.Errorf("%s: %w", methodName, err)
+		}
+
+		err = tx.Commit()
+		if err != nil {
+			return fmt.Errorf("%s: %w", methodName, err)
+		}
+
+		return nil
+	})()
 }
 
 // Do a wrapper for database requests.
 // If the callback returns the error, it will be wrapped and enriched with
 // information about where the transaction was called from.
-// TODO: Add collect metric.
+// Automatically collects metrics for function calls.
 func (r *Repo) Do(fn func(*sql.DB) error) error {
 	methodName := callerMethodName()
 
-	err := fn(r.db)
-	if err != nil {
-		return fmt.Errorf("%s: %w", methodName, err)
-	}
-
-	return nil
+	return r.metricCollector.collect(methodName, func() error {
+		err := fn(r.db)
+		if err != nil {
+			return fmt.Errorf("%s: %w", methodName, err)
+		}
+		return nil
+	})()
 }
